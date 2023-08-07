709373e2cb224d79db34cc86530cac06e9f5bb53
Zithular <141595276+Zithular@users.noreply.github.com>
2023-08-07 02:01:51 +0100

    Create install.bat

---
+++ install.bat
@@ -0,0 +1,13 @@
+@echo off
+
+echo Installing pywin32...
+pip install pywin32
+
+if %errorlevel% NEQ 0 (
+    echo Failed to install pywin32. Please check the error message above.
+) else (
+    echo pywin32 installed successfully.
+)
+
+pause
+exit
