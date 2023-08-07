9d82d1c552196664a731857c9b81188c1e89c929
Zithular <141595276+Zithular@users.noreply.github.com>
2023-08-07 23:38:05 +0100

    Update and rename install.bat to Install.bat

--- Install.bat
+++ Install.bat
@@ -1,7 +1,7 @@
 @echo off
 
 echo Installing pywin32...
-pip install pywin32
+pip install pywin32 > nul
 
 if %errorlevel% NEQ 0 (
     echo Failed to install pywin32. Please check the error message above.
